79fd785dfa86a1c43a688e13917ecb048505afbc
burnbrownbear21 <burnbrownbear21@gmail.com>
2023-05-29 11:56:23 +0700

    Object

---
+++ 2lab.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+
+void path(int c, int x, int y)
+{
+    c = sqrt(pow(x,2)+pow(y,2));
+    cout<<"full path: "<<c<<endl;
+}
+int main()
+{
+    srand(time(NULL));
+    int x=8, y = 0, c = 0,  n;
+    cout<<"enter value: " <<endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int z=rand()%10;
+        x=x+z;
+        y=y+z;
+        cout<<"value "<<x<<"/"<<y<<endl;
+        if (i==0 || i == n-1)
+        {
+            path (c,x,y);
+        }
+
+    }
+
+    return 0;
+}
